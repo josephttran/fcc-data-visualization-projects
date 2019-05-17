aafc6c12e87601327ab4784b4d66042770bf82c6
josephttran <42635012+josephttran@users.noreply.github.com>
2019-05-17 00:28:38 -0700

    Add Scalable Vector Graphics, chart title

--- bar-chart/bar-chart.js
+++ bar-chart/bar-chart.js
@@ -15,4 +15,39 @@
 
 function displayChart(dataObject) {
   console.log(dataObject);
+
+  const containerWidth = 600;
+  const containerHeight = 300;
+
+  const svg = d3.select('#chart-container')  
+      .append('svg')
+      .attr('viewBox', `0 0 ${containerWidth} ${containerHeight}`)
+      
+  const title = dataObject.name
+  displayTitle(svg, title);
+}
+
+function displayTitle(svg, dataTitle) {
+  console.log(dataTitle);
+
+  const titleFontSize = 'calc(12px + 2vmin)';
+  const titlePadding = 20
+  const titleBackgroundHeight = 50;
+  
+  // Chart title background
+  svg.append('rect')
+      .attr('id', 'title-bg')
+      .attr('width', '100%')
+      .attr('height', titleBackgroundHeight)
+      .attr('fill', '#a0ffef')
+  
+  // Chart title 
+  svg.append('text')
+      .text(dataTitle)
+      .attr('id', 'title')
+      .attr('fill', '#205899')
+      .attr('x', '50%')
+      .attr('text-anchor', 'middle')
+      .attr('y', titleBackgroundHeight / 2 + titlePadding / 2)
+      .attr('font-size', titleFontSize);
 }
