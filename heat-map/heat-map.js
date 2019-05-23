eb3500baa054e8ca04b0dc514e599f43f253aaa2
josephttran <42635012+josephttran@users.noreply.github.com>
2019-05-23 16:05:40 -0700

    Display heat map heading

--- heat-map/heat-map.js
+++ heat-map/heat-map.js
@@ -3,7 +3,6 @@
 
   try {
     const res = await fetch(url);
-
     const data = await res.json();
     displayHeatMap(data);
   } catch(err) {
@@ -16,8 +15,39 @@ function displayHeatMap(data) {
   const heatMap = d3.select('#heat-map');
   const mapWidth = 600;
   const mapHeight = 320;
+  const minMaxYear = getMinMaxYear(data);
+  const titleBgHeight = 80;
+  const titleName = 'Monthly Global Land-Surface Temperature'
+  const description = `${minMaxYear[0]} - ${minMaxYear[1]}: base temperature ${data['baseTemperature']} ℃`;
 
   const svg = heatMap.append('svg')
       .attr('viewBox', `0 0 ${mapWidth} ${mapHeight}`);
 
+  displayHeading(svg, titleBgHeight, titleName, description);
+
+}
+
+function displayHeading(svg, titleBgHeight, titleName, description) {
+  const titleFontSize = titleBgHeight / 4;
+  // Title
+  svg.append('text')
+      .text(titleName)
+      .attr('id', 'title')
+      .attr('x', '50%')
+      .attr('y', titleBgHeight / 3)
+      .style('text-anchor', 'middle')
+      .style('font-size', titleFontSize);
+      
+  // Description
+  svg.append('text')
+      .text(description)
+      .attr('id', 'description')
+      .attr('x', '50%')
+      .attr('y', titleBgHeight * 2 / 3)
+      .style('text-anchor', 'middle')
+      .style('font-size', titleFontSize - 4);
+}
+    
+function getMinMaxYear(data) {
+  return d3.extent(data['monthlyVariance'].map(obj => obj['year']));
 }
