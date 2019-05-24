27fe204ffa4d7e192f178a3d31413dd4efdb7b9b
josephttran <42635012+josephttran@users.noreply.github.com>
2019-05-23 17:56:05 -0700

    Display x-axis and y-axis

--- heat-map/heat-map.js
+++ heat-map/heat-map.js
@@ -15,16 +15,24 @@ function displayHeatMap(data) {
   const heatMap = d3.select('#heat-map');
   const mapWidth = 600;
   const mapHeight = 320;
-  const minMaxYear = getMinMaxYear(data);
+  const axisPadding = {
+    top: 10,
+    left: 60,
+    right: 10,
+    bottom: 80,
+  };  
   const titleBgHeight = 80;
-  const titleName = 'Monthly Global Land-Surface Temperature'
+  const titleName = 'Monthly Global Land-Surface Temperature';
+  const minMaxYear = getMinMaxYear(data);
   const description = `${minMaxYear[0]} - ${minMaxYear[1]}: base temperature ${data['baseTemperature']} ℃`;
 
   const svg = heatMap.append('svg')
       .attr('viewBox', `0 0 ${mapWidth} ${mapHeight}`);
 
-  displayHeading(svg, titleBgHeight, titleName, description);
+  const axis = getHeatMapAxis(data['monthlyVariance'], mapWidth, mapHeight, axisPadding, titleBgHeight);
 
+  displayHeading(svg, titleBgHeight, titleName, description);
+  displayAxis(svg, mapHeight, axis, axisPadding);
 }
 
 function displayHeading(svg, titleBgHeight, titleName, description) {
@@ -50,4 +58,41 @@ function displayHeading(svg, titleBgHeight, titleName, description) {
     
 function getMinMaxYear(data) {
   return d3.extent(data['monthlyVariance'].map(obj => obj['year']));
+}
+
+function getHeatMapAxis(dataObjArr, svgWidth, svgHeight, axisPadding, titleBgHeight) {
+  // X-axis
+  const xDomain = d3.extent(dataObjArr.map(obj => obj['year']));
+  const xRange = [axisPadding.left , svgWidth - axisPadding.right];
+  const xScale = d3.scaleLinear(xDomain, xRange);
+  const xAxis = d3.axisBottom(xScale).tickFormat(year => year);
+  // Y-axis
+  const yDomain = [new Date('January 1, 2000'), new Date('December 31, 2000')];
+  const yRange = [svgHeight - axisPadding.bottom, titleBgHeight + axisPadding.top];
+  const yScale = d3.scaleTime(yDomain, yRange);
+  const timeFormat = d3.timeFormat('%B')
+  const yAxis = d3.axisLeft(yScale).tickFormat(d => timeFormat(d));
+
+  const axis = {
+    scales: {
+      x: xScale,
+      y: yScale,
+    },
+    xAxis: xAxis,
+    yAxis: yAxis,
+  };
+
+  return axis;
+}
+
+function displayAxis(svg, svgHeight, axis, axisPadding) {
+  svg.append('g')
+      .attr('id', 'x-axis')
+      .attr('transform', `translate(0, ${svgHeight - axisPadding.bottom})`)
+      .call(axis.xAxis);
+
+  svg.append('g')
+      .attr('id', 'y-axis')
+      .attr('transform', `translate(${axisPadding.left}, 0)`)
+      .call(axis.yAxis);
 }
