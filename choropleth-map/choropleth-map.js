3d81af55ecacde3cb5108a246cfc5f610107f695
josephttran <42635012+josephttran@users.noreply.github.com>
2019-05-28 13:09:06 -0700

    Add tooltip

--- choropleth-map/choropleth-map.js
+++ choropleth-map/choropleth-map.js
@@ -31,6 +31,9 @@ function displayChoroplethMap(countyData, educationData) {
   const titleName = 'United States Educational Attainment';
   const description = `Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)`;
   const paddingLeft = 200;
+  const tooltip = d3.select('body').append('div')
+      .attr('id', 'tooltip')
+      .style('opacity', 0);
 
   const svg = choroplethMap.append('svg')
       .attr('viewBox', `0 0 ${mapWidth} ${mapHeight}`);
@@ -41,6 +44,7 @@ function displayChoroplethMap(countyData, educationData) {
   displayHeading(svg, titleBgHeight, titleName, description);
   displayCounties(svg, countiesGeoJson, paddingLeft);
   addEducationDataToCounties(svg, educationData);
+  displayTooltip(svg, tooltip);
 }
 
 function displayHeading(svg, titleBgHeight, titleName, description) {
@@ -96,4 +100,36 @@ function addEducationDataToCounties(svg, educationData) {
             .attr('data-fips', educationData[objIndex]['fips'])
             .attr('data-education', educationData[objIndex]['bachelorsOrHigher'])
       });
+}
+
+function displayTooltip(svg, tooltip) {
+  svg.selectAll('.county')
+      .on('mouseover', function(obj) {
+        const tooltipLeftMarginPadding = parseInt(tooltip.style('padding')) + parseInt(tooltip.style('margin'));
+        const tooltipTopBottomMarginPadding = (parseInt(tooltip.style('padding')) * 2) 
+            + (parseInt(tooltip.style('margin')) * 2);
+
+        const tooltipHeight = parseInt(tooltip.style('height')) + tooltipTopBottomMarginPadding;
+        const tooltipWidth = parseInt(tooltip.style('width')) / 2 + tooltipLeftMarginPadding;
+
+        const left = d3.event.pageX - tooltipWidth  + 'px';
+        const top = d3.event.pageY - tooltipHeight + 'px';
+
+        const htmlText = `${obj['area_name']}, ${obj['state']}</br>
+          ${obj['bachelorsOrHigher']}%
+        `;       
+
+        tooltip.html(htmlText)
+            .attr('data-education', obj['bachelorsOrHigher'])
+            .style('opacity', 1)
+            .style('top', top)
+            .style('left', left)
+            .transition()
+            .duration(200);
+      })
+      .on('mouseout', function() {
+        tooltip.style('opacity', 0)
+            .transition()
+            .duration(200);
+      })
 }
